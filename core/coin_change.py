3a851370cfcf549dfc4b73793c672641c1f45f2f
cecegoretti <cece.goretti@gmail.com>
2026-01-07 09:42:46 +0100

    Update coin_change.py

--- core/coin_change.py
+++ core/coin_change.py
@@ -9,30 +9,30 @@ def coin_change(coin_values, amount):
       the amounts that can be obtained from the set 
       of coins.
       Space complexity: O(amount)
-      Time complexity: O(amount*len(coin_values).
+      Time complexity: O(amount*len(coin_values)).
       Note: if gcd of coin_values is >1, than BFS approach
       is optimal compared to classical DP. If gcd=1 and
       amount big enough, the two approach are computationally
       equivalent.
       '''
-  if amount<0:
-    return -1
-  if amount==0:
-    return 0
-  dp=[0]*(amount+1)
-  #dp[i]==0 means either i=0 (reachable with 0 coins) or
-  #i is not visited yet
-  new_found=deque([0])
-  while new_found:
-    last_found=new_found.popleft()
-    for i in coin_values:
-      if last_found+i<=amount:
-        if dp[last_found+i]==0:
-          #last_found+i>0 hence we are checking 
-          #if last_found+i was visited
-          dp[last_found+i]=dp[last_found]+1
-          new_found.append(last_found+i)
-  if dp[amount]==0:
-    return -1
-  else:
-    return dp[amount]
+    if amount<0:
+        return -1
+    if amount==0:
+        return 0
+    dp=[0]*(amount+1)
+    #dp[i]==0 means either i=0 (reachable with 0 coins) or
+    #i is not visited yet
+    new_found=deque([0])
+    while new_found:
+        last_found=new_found.popleft()
+        for i in coin_values:
+            if last_found+i<=amount:
+                if dp[last_found+i]==0:
+                  #last_found+i>0 hence we are checking 
+                  #if last_found+i was visited
+                  dp[last_found+i]=dp[last_found]+1
+                  new_found.append(last_found+i)
+    if dp[amount]==0:
+        return -1
+    else:
+        return dp[amount]
