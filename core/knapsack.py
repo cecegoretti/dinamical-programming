e13953cbc54372a8a2e5b6c1612b6d5bdfefa133
cecegoretti <cece.goretti@gmail.com>
2026-01-07 09:39:51 +0100

    Update knapsack.py

--- core/knapsack.py
+++ core/knapsack.py
@@ -1,5 +1,3 @@
-#knapsack problem, given arrays values and weights, find the highest possible value by adding elements with the constraint total_weight<max_weight
-
 def optimalfilling(values, weights, max_weight):
   """
     0/1 Knapsack problem.
@@ -10,15 +8,15 @@ def optimalfilling(values, weights, max_weight):
     Time complexity: O(n * W)
     Space complexity: O(n * W)
     """
-  dp=[[0]*len(values) for _ in range(max_weight+1)]
-  #dp[j][i] saves the optimal filling with maximal capacity j using the first i elements
-  for j in range(max_weight+1):
-    if weights[0]<=j:
-      dp[j][0]=values[0]
-  for i in range(1,len(values)):
+    dp=[[0]*len(values) for _ in range(max_weight+1)]
+    #dp[j][i] saves the optimal filling with maximal capacity j using the first i elements
     for j in range(max_weight+1):
-      if weights[i]<=j:
-        dp[j][i]=max(dp[j-weights[i]][i-1]+values[i],dp[j][i-1])
-      else:
-        dp[j][i]=dp[j][i-1]
-  return dp[max_weight][len(values)-1]
+        if weights[0]<=j:
+            dp[j][0]=values[0]
+    for i in range(1,len(values)):
+        for j in range(max_weight+1):
+            if weights[i]<=j:
+                dp[j][i]=max(dp[j-weights[i]][i-1]+values[i],dp[j][i-1])
+            else:
+                dp[j][i]=dp[j][i-1]
+    return dp[max_weight][len(values)-1]
