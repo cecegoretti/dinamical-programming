85ec99dd4f8bafc2c8247eaf68c961a5c978a963
cecegoretti <cece.goretti@gmail.com>
2026-01-07 09:47:12 +0100

    Update longest_increasing_subsequence.py

--- core/longest_increasing_subsequence.py
+++ core/longest_increasing_subsequence.py
@@ -1,13 +1,17 @@
-#compute the lenght of the longest increasing subsequence (not substring)
-#leetcode exercise: https://leetcode.com/problems/longest-increasing-subsequence/description/?envType=problem-list-v2&envId=dynamic-programming
-#dp[i] saves the lenght of the longest increasing subsequence ending in position i
-#memory used: len(nums), time complexity: len(nums)**2
-
-class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1] * len(nums)
-        for j in range(1, len(nums)):
-            for i in range(j):
-                if nums[j] > nums[i]:
-                    dp[j] = max(dp[j], dp[i] + 1)
-        return max(dp)
+def lengthOfLIS(self, nums: List[int]) -> int:
+    '''
+    Compute the lenght of the longest increasing subsequence (not substring)
+    of nums. 
+    It uses a dinamical programming approach:
+    dp[i] saves the lenght of the longest increasing 
+    subsequence ending in position i.
+    
+    Space complexity: O(len(nums))
+    Time complexity: O(len(nums)**2)
+    '''
+    dp = [1] * len(nums)
+    for j in range(1, len(nums)):
+        for i in range(j):
+            if nums[j] > nums[i]:
+                dp[j] = max(dp[j], dp[i] + 1)
+    return max(dp)
