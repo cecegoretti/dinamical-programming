a4a15e8e16244766d9ee0b8818447b3efb128885
cecegoretti <cece.goretti@gmail.com>
2026-01-11 19:02:11 +0100

    Update and rename combination_sum.py to additionalcombination_sum.py

--- core/additionalcombination_sum.py
+++ core/additionalcombination_sum.py
@@ -3,9 +3,11 @@ exercise: https://leetcode.com/problems/combination-sum-iv/
 def combinationSum4(self, nums: List[int], target: int) -> int:
 '''  
   This function computes in how many different ways
-  one can obtain amount by adding elements of nums.
+  one can obtain the value amount by adding elements of nums.
   Sequences that differ in the order are counted
   as different.
+  Space complexity: O(len(nums))
+  Time complexity: O(len(nums)*target)
 '''
   n=len(nums)
   dp=[0]*(target+1)
